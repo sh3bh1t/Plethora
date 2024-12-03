4239a035adb8a23dbda67df29f8910188dfa158d
sh3bh1t <shobhits2000@gmail.com>
2024-12-03 15:02:28 +0530

    user db model setup

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,27 @@
-const dotenv=require('dotenv');
+const dotenv = require('dotenv');
 dotenv.config();
-const cors=require('cors');
-const express=require ('express');
-const app=express();
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
 
 app.use(cors);
 
-const port=process.env.PORT||8080;
+const port = process.env.PORT || 8080;
+const mongo_uri=process.env.MONGO_URL;
+
+main().then(() => {
+    console.log('connected to db')
+})
+.catch((err) => {
+        throw err;
+    });
 
 
-app.listen(port,()=>{
+async function main() {
+    await mongoose.connect(mongo_uri);
+}
+
+app.listen(port, () => {
     console.log(`server is listening to port ${port}`);
 })
---
+++ backend/models/user.model.js
@@ -0,0 +1,47 @@
+const mongoose=require('mongoose');
+const bcrypt=require('bcrypt');
+const jwt=require('jsonwebtoken');
+
+
+const userSchema = new mongoose.Schema({
+    fullname :{
+        firstname:{
+            type:String,
+            required:true,
+            minlength:[3,'first name must be atleast 3 letters']
+        },
+        lastname:{
+            type:String,
+            minlength:[3,'last name must be atleast 3 letters']
+        },
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+        select:false,
+    },
+    socketId:{
+        type:String,
+    },
+})
+
+userSchema.methods.generateAuthToken = () =>{
+    const token = jwt.sign({_id:this._id},process.env.JWT_SECRET);
+    return token;
+}
+
+userSchema.methods.comparePassword= async (password)=>{
+    return await bcrypt.compare(password,this.password);
+}
+
+userSchema.methods.hashPassword= async (password) =>{
+    return await bcrypt.hash(password,10);
+}
+
+const User= mongoose.model('User', userSchema);
+module.exports=User;
