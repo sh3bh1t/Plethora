4425fb7bbee71a18e70bf0c293b940531f11bd96
sh3bh1t <shobhits2000@gmail.com>
2024-12-03 14:23:34 +0530

    basic server setup

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,13 @@
+const dotenv=require('dotenv');
+dotenv.config();
+const cors=require('cors');
 const express=require ('express');
 const app=express();
 
-const port=8080;
+app.use(cors);
+
+const port=process.env.PORT||8080;
+
 
 app.listen(port,()=>{
     console.log(`server is listening to port ${port}`);
