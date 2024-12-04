3d243037c11b8257064f801a5714db3963e6db3d
sh3bh1t <shobhits2000@gmail.com>
2024-12-05 00:30:06 +0530

    homepage built

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import UserSignup from './pages/UserSignup'
+import UserLogin from './pages/UserLogin'
+import DriverSignup from './pages/DriverSignup'
+import DriverLogin from './pages/DriverLogin'
 
 const App = () => {
   return (
     <div >
       <Routes>
         <Route path='/' element={<Home/>} />
+        <Route path='/u/signup' element={<UserSignup/>} />
+        <Route path='/u/login' element={<UserLogin/>} />
+        <Route path='/d/signup' element={<DriverSignup/>} />
+        <Route path='/d/login' element={<DriverLogin/>} />
       </Routes>
     </div>
   )
