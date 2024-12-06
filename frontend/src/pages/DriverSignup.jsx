53c8f423e4e1f8e8454d9581e05d5bcd30f3dc71
sh3bh1t <shobhits2000@gmail.com>
2024-12-06 17:42:49 +0530

    signup pages build for driver and user

--- frontend/src/pages/DriverSignup.jsx
+++ frontend/src/pages/DriverSignup.jsx
@@ -1,9 +1,91 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+
 
 const DriverSignup = () => {
+
+  const [email, setEmail] = useState(' ');
+  const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [vehicleData,setVehicleData]= useState({});
+  const [driverSignupData, setDriverSignupData] = useState({});
+
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setDriverSignupData({
+      email: email,
+      password: password,
+      fullname: {
+        firstName: firstName,
+        lastName: lastName,
+      }
+      // vehicleData:{
+        
+      // }
+    });
+    setEmail('');
+    setPassword('');
+    setFirstName('');
+    setLastName('');
+    // console.log(userSignupData);
+  }
+
+
   return (
-    <div>DriverSignup</div>
+    <div className='p-7 h-screen flex flex-col justify-between'>
+      <div>
+        <img className='w-full mb-1' src="/images/cover-removebg-preview.png" alt="_plethora_logo" />
+        <form onSubmit={(e) => {
+          submitHandler(e)
+        }}>
+          <h3 className='text-xl mb-2 font-medium'>What's your name?</h3>
+          <div className='flex gap-4 '>
+            <input type="text"
+              placeholder='first name'
+              required
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              className='text-lg bg-[#eeeeee] rounded border px-4 py-2 mb-2 w-1/2 placeholder:text-sm '
+            />
+            <input type="text"
+              placeholder='last name'
+              required
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              className='text-lg bg-[#eeeeee] rounded border px-4 py-2 mb-2 w-1/2 placeholder:text-sm '
+            />
+          </div>
+          <h3 className='text-xl mb-2 font-medium'>What's your email?</h3>
+          <input type="email"
+            placeholder='example@example.com'
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className='text-lg bg-[#eeeeee] rounded border px-4 py-2 mb-2 w-full placeholder:text-sm '
+          />
+          <h3 className='mt-2 mb-2 text-xl font-medium'>Enter Password</h3>
+          <input type="password"
+            placeholder='enter password here'
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className=' border rounded text-lg placeholder:text-sm bg-[#eeeeee] w-full px-4 py-2 mb-7'
+          />
+          <button className='flex items-center justify-center w-full bg-black text-white py-3 rounded mt-1 mb-2 text-lg'
+          >Sign Up <FontAwesomeIcon className='pl-2' icon={faArrowRight} /></button>
+          <Link to={'/d/login'} className='text-blue-600 text-center  px-10 mt-2 '>Already have an account? Sign In!</Link>
+        </form>
+      </div>
+      <div>
+        <p className='text-[10px] leading-tight'>By agreeing to continue you consent to company privacy policies and usage and agree to recive calls,texts/SMSs from company and it's affiliates</p>
+      </div>
+    </div>
   )
 }
-
+ReactDOM.render(<Router>< DriverSignup /></Router>, document.getElementById('root'));
 export default DriverSignup
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Link } from 'react-router-dom';
 const Home = () => {
     return (
         <div>
-            <div className='bg-cover bg-center bg-[url(https://images.unsplash.com/photo-1569542609987-2c0e108c8bc9?q=80&w=1885&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] h-screen pt-8 flex justify-between flex-col w-full bg-red-400'>
+            <div className='bg-cover bg-center bg-[url(https://images.unsplash.com/photo-1569542609987-2c0e108c8bc9?q=80&w=1885&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] h-screen pt-8 flex justify-between flex-col w-full '>
                 <img className='w-20  ml-6' src="/images/cover-removebg-preview.png" alt="Plethora_Logo" />
                 <div className='bg-white  py-4 px-10 pb-7 text-center'>
                     <h2 className='text-3xl font-bold'>Get Started with Plethora</h2>
